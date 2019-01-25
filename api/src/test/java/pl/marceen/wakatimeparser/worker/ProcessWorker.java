8b7c6cd7ee348935530d91a2a6e48039d3d2e54a
Marcin Zaremba <marcin.zaremba@bm.pl>
2019-01-25 16:58:23 +0100

    #3 - login - step two incomplete

--- api/src/test/java/pl/marceen/wakatimeparser/worker/ProcessWorker.java
+++ api/src/test/java/pl/marceen/wakatimeparser/worker/ProcessWorker.java
@@ -14,6 +14,9 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 
+import static java.net.URLEncoder.encode;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 /**
  * @author Marcin Zaremba
  */
@@ -36,6 +39,7 @@ class ProcessWorker {
         logger.info("Config: {}", config);
 
         HttpClient httpClient = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.NORMAL)
                 .build();
 
         logger.info("Getting csrf token");
@@ -48,7 +52,15 @@ class ProcessWorker {
 
         String csrfToken = loginFormParser.findCsrfToken(responseLoginForm);
 
-        // TODO: 2019-01-20 login into panel (POST with form)
+        String responseLogin = httpClient.sendAsync(
+                        buildRequestForLogin(csrfToken, config.getLogin(), config.getPassword()),
+                        HttpResponse.BodyHandlers.ofString()
+                )
+                .thenApply(HttpResponse::body)
+                .get();
+
+        logger.info("Response: {}", responseLogin);
+//        write("login.html", responseLogin);
     }
 
     private AuthConfig convertJsonToAuthConfig(String json) {
@@ -59,6 +71,31 @@ class ProcessWorker {
         return HttpRequest.newBuilder()
                 .uri(URI.create("https://wakatime.com/login"))
                 .timeout(Duration.ofMinutes(1))
+                .build();
+    }
+
+    private HttpRequest buildRequestForLogin(String csrfToken, String login, String password) {
+        String post = String.format("csrftoken=%s&email=%s&password=%s", csrfToken, encode(login, UTF_8), encode(password, UTF_8));
+
+        logger.info("POST: {}", post);
+
+        return HttpRequest.newBuilder()
+                .uri(URI.create("https://wakatime.com/login"))
+//                .timeout(Duration.ofMinutes(1))
+                .headers(
+//                        "Host", "wakatime.com",
+                        "User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0",
+                        "Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                        "Accept-Language", "pl,en-US;q=0.7,en;q=0.3",
+                        "Accept-Encoding", "gzip, deflate, br",
+//                        "Referer", "https://wakatime.com/",
+                        "Content-Type", "application/x-www-form-urlencoded",
+                        "Upgrade-Insecure-Requests", "1",
+                        "Pragma", "no-cache",
+                        "Cache-Control", "no-cache",
+                        "TE", "Trailers"
+                )
+                .POST(HttpRequest.BodyPublishers.ofString(post))
                 .build();
     }
 }
